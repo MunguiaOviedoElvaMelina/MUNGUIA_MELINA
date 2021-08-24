cc5085ce992cc2bf55e248d213006196a312c1cb
MunguiaOviedo <emunguia6@ucol.mx>
2021-08-24 17:18:19 -0500

    practica_1_chalk

---
+++ app.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.red('Hello world!'));
+console.log(chalk.green('Hello world!'));
+console.log(chalk.magenta('Hello world!'));
+console.log(chalk.cyan('Hello world!'));
+console.log(chalk.yellow('Hello world!'));
